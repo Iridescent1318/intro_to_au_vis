e1309b890d8bf35cbbe105759bda8f3449de6be2
Iridescent1318 <lzty19981106@qq.com>
2019-11-08 23:32:22 +0800

    (Maybe) final version

--- project01/HeightEstimator.m
+++ project01/HeightEstimator.m
@@ -1,13 +1,14 @@
 function H = HeightEstimator(img,mask_ground,mask_target,mask_person1,mask_person2,ref_height1,ref_height2,point_target,lines)
     imshow(img)
     [H,W,~] = size(img);
+    max_lines_nums = 8;
     hold on
-    drawLines(lines, W, 12)
-    [vps, vps_class, vph] = get_vps(lines, 12, H, W);
+    draw_lines(lines, W, max_lines_nums)
+    vph = get_vps(lines, max_lines_nums, H, W);
     scatter(point_target(1), point_target(2), 'x')
-    p1_xy = drawPersonLine(mask_person1);
-    p2_xy = drawPersonLine(mask_person2);
-    t_xy = drawTargetLine(mask_ground, point_target(1:2));
+    p1_xy = draw_person_line(mask_person1);
+    p2_xy = draw_person_line(mask_person2);
+    t_xy = draw_target_line(mask_ground, point_target(1:2));
 
     p1_line = cross(p1_xy(:, 1), p1_xy(:, 2));
     p1_line = p1_line / p1_line(3);
@@ -16,87 +17,99 @@ function H = HeightEstimator(img,mask_ground,mask_target,mask_person1,mask_perso
     t_line = cross(t_xy(:, 1), t_xy(:, 2));
     t_line = t_line / t_line(3);
 
-    vpv1 = estimateVpv(p1_line, t_line);
-    vpv2 = estimateVpv(p2_line, t_line);
+    vpv1 = estimate_vpointv(p1_line, t_line);
+    vpv2 = estimate_vpointv(p2_line, t_line);
+    vpv = (vpv1+vpv2)/2;
 
-    [p1_t, p1_b] = selectTopBottom(p1_xy);
-    [p2_t, p2_b] = selectTopBottom(p2_xy);
-    [t_t, t_b] = selectTopBottom(t_xy);
+    [p1_t, p1_b] = select_top_bottom(p1_xy);
+    [p2_t, p2_b] = select_top_bottom(p2_xy);
+    [t_t, t_b] = select_top_bottom(t_xy);
 
-    H1 = estimateHeight(vpv1, vph, p1_t, p1_b, ref_height1, t_t, t_b);
-    H2 = estimateHeight(vpv2, vph, p2_t, p2_b, ref_height2, t_t, t_b);
-    H = (H1 + H2)/2
+    H1 = estimate_height(vpv, vph, p1_t, p1_b, ref_height1, t_t, t_b);
+    H2 = estimate_height(vpv, vph, p2_t, p2_b, ref_height2, t_t, t_b);
+    H0 = (H1 + H2)/2
 
-    function [vps, vps_class, vph] = get_vps(lines, max_lines_nums, H, W)
+    function vph = get_vps(lines, max_lines_nums, H, W)
         vps = [];
-        for i=1:min(length(lines), max_lines_nums)
-            for j=i:min(length(lines), max_lines_nums)
+        alpha = 1.65;
+        line_num = min(length(lines(:, 1)), max_lines_nums);
+        for i=1:line_num
+            for j=i:line_num
                 if(i == j)
                     continue;
                 end
                 nvps = cross(lines(i, :), lines(j, :));
                 nvps = nvps / nvps(3);
-                vps = [vps;nvps(1:2)];
+                vps = [vps;nvps];
             end
         end
-        vps = vps';
-        to_del = zeros(1, length(vps(1, :)));
-        for i=1:length(to_del)
-            if(vps(1, i) > 0 && vps(1, i) < W && vps(2, i) > 0 && vps(2, i) < H)
-                to_del(i) = 1;
+
+        [~, score, ~] = pca(vps(:, 1:2));
+        [~, s_sort_idx] = sort(score(:, 2));
+        vps_num = 0;
+        if(mod(line_num, 2) == 0)
+            vps_num = line_num^2/4-line_num/2;
+        else
+            vps_num = (line_num-1)^2/4-1;
+        end
+
+    %     vps_color = zeros(1, length(vps(:, 1)));
+    %     vps_color(s_sort_idx(1:vps_num)) = 65535;
+    %     figure
+    %     scatter(score(:, 1), score(:, 2), 30, vps_color);
+
+        vps = vps(s_sort_idx(1:vps_num), 1:3);
+        vps_class = kmeans(vps(:, 1:2), 2);
+        vps_class_idx_1 = find(vps_class == 1);
+        vps_class_idx_2 = find(vps_class == 2);
+
+        l_bs = [];
+        for i=1:length(vps_class_idx_1)
+            for j=1:length(vps_class_idx_2)
+                newline = cross(vps(vps_class_idx_1(i), :), vps(vps_class_idx_2(j), :));
+                newline = newline / newline(3);
+                nl_b = -1/newline(2);
+                l_bs = [l_bs, nl_b];
             end
         end
-        vps(:, find(to_del == 1)) = [];
-        color = [[1,0,0];[0,1,0];[0,0,1];[1,1,0];[1,0,1];[0,1,1]];
-        vps_class = kmeans(vps', 2);
-        scatter(vps(1, :), vps(2, :), 144, color(vps_class, :), '.')
+        sigma = std(l_bs);
 
-        nearest_dis = zeros(1, length(vps(1, :)));
-        for i=1:length(nearest_dis)
-             nearest_d = Inf;
-            for j=1:length(nearest_dis)
-                if(i == j)
-                    continue;
-                end
-                if((vps(1, i)-vps(1, j))^2 + (vps(2, i)-vps(2, j))^2 < nearest_d)
-                    nearest_d = (vps(1, i)-vps(1, j))^2 + (vps(2, i)-vps(2, j))^2;
-                end
-            end
-            nearest_dis(i)= nearest_d;
-        end  
-        nearest_dis = 1 ./ nearest_dis;
+        d_weight = ones(length(vps(:, 1)), 1);
 
-        partial_k_k = sum( nearest_dis.*vps(1,:).^2 );
-        partial_k_b = sum( nearest_dis.*vps(1,:) );
-        partial_k_c = sum( nearest_dis.*vps(1,:).*vps(2,:) );
+        vps_x = vps(:, 1);
+        vps_y = vps(:, 2);
+        partial_k_k = sum( d_weight.*vps_x.^2 );
+        partial_k_b = sum( d_weight.*vps_x );
+        partial_k_c = sum( d_weight.*vps_x.*vps_y );
         partial_b_k = partial_k_b;
-        partial_b_b = sum( nearest_dis );
-        partial_b_c = sum( nearest_dis.*vps(2,:) );
+        partial_b_b = sum( d_weight );
+        partial_b_c = sum( d_weight.*vps_y );
         A = [partial_k_k, partial_k_b;partial_b_k, partial_b_b];
         B = [partial_k_c; partial_b_c];
         C = A\B;
         m = C(1);
         b = C(2);
-        x0 = -2000:3000;
-        y0 = m.*x0+b;
-        vph = [m/b, -1/b, 1];
-        plot(x0, y0, 'Color', 'c', 'LineWidth', 3);
+
+        op_m = m;
+        op_b = b + sigma * alpha;
+
+        x0 = -3000:3000;
+        y0 = op_m.*x0+op_b;
+        vph = [op_m/op_b, -1/op_b, 1];
+        plot(x0, y0, 'Color', 'g', 'LineWidth', 3);
     end
 
-    function drawLines(lines, W, max_lines_nums)
+    function draw_lines(lines, W, max_lines_nums)
         k = -lines(:, 1)./lines(:, 2);
         b = -lines(:, 3)./lines(:, 2);
-        c_index = kmeans(k, 2);
         for i=1:min(length(lines), max_lines_nums)
             x = -2.5*W:2.5*W;
             y = k(i).*x+b(i);
-            color = ['r', 'g', 'b', 'c', 'm', 'y'];
-            c = c_index(i);
-            plot(x, y, 'Color', color(c), 'LineWidth', 1)
+            plot(x, y, 'Color', 'r', 'LineWidth', 1)
         end
     end
 
-    function person_xy = drawPersonLine(person_mask)
+    function person_xy = draw_person_line(person_mask)
         plist = ones(sum(sum(person_mask)), 2);
         plist_k = 1;
         for i=1:length(person_mask(1, :))
@@ -128,7 +141,7 @@ function H = HeightEstimator(img,mask_ground,mask_target,mask_person1,mask_perso
         plot(person_x, person_y, 'Color', 'y', 'LineWidth', 2);
     end
 
-    function target_xy = drawTargetLine(ground_mask, target_point)
+    function target_xy = draw_target_line(ground_mask, target_point)
         d_min = Inf;
         d_min_point = [0, 0];
         for i=1:length(ground_mask(1, :))
@@ -145,16 +158,16 @@ function H = HeightEstimator(img,mask_ground,mask_target,mask_person1,mask_perso
         slope = (target_point(2) - d_min_point(2)) / (target_point(1) - d_min_point(1));
         target_y = slope.*(target_x - target_point(1)) + target_point(2);
         target_xy = [target_x(1), target_x(end); target_y(1), target_y(end); 1, 1];
-        plot(target_x, target_y, 'Color', 'm', 'LineWidth', 1.5);
+        plot(target_x, target_y, 'Color', 'b', 'LineWidth', 1.5);
     end
 
-    function vpv = estimateVpv(line1, line2)
+    function vpv = estimate_vpointv(line1, line2)
         vpv = cross(line1, line2);
         vpv = vpv / vpv(3);
         scatter(vpv(1), vpv(2), 'p')
     end
 
-    function [top, bottom] = selectTopBottom(xy)
+    function [top, bottom] = select_top_bottom(xy)
         if(xy(2, 1) > xy(2, 2))
             top = xy(:, 2);
             bottom = xy(:, 1);
@@ -164,7 +177,7 @@ function H = HeightEstimator(img,mask_ground,mask_target,mask_person1,mask_perso
         end
     end
 
-    function target_height = estimateHeight(vpoint_vertical, vline, ref_top, ref_bottom, ref_height, target_top, target_bottom)
+    function target_height = estimate_height(vpoint_vertical, vline, ref_top, ref_bottom, ref_height, target_top, target_bottom)
         % all inputs are in (x, y, 1) form
         b_r = cross(ref_bottom, ref_top);
         b_r = b_r / b_r(3);
