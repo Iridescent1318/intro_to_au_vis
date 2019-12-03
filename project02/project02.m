41cc007f4ab3ee93ebc03d02dff36612c62a8dfc
Iridescent1318 <lzty19981106@qq.com>
2019-12-03 20:40:04 +0800

    Final Version

--- project02/project02.m
+++ project02/project02.m
@@ -1,10 +1,25 @@
 clear;clc
 
-ground_truth = textread("./train/angle.txt");
-deg = ones(1, 14);
+TRAINING_SET = 1;
+% For training result, please set TRAINING_SET to 1
+% For test output, set TRAINING_SET to 0
+% You may change the directory below (PATH_NAME)
 
-for n = 1:14
-    [y, Fs] = audioread("./train/"+mat2str(n)+".wav");
+sample_num = 0;
+PATH_NAME = "";
+if(TRAINING_SET == 1)
+    sample_num = 14;
+    ground_truth = textread("./train/angle.txt");
+    PATH_NAME = "./train/";
+else
+    sample_num = 140;
+    PATH_NAME = "./test/";
+end
+deg_init = ones(1, sample_num);
+deg = ones(1, sample_num);
+
+for n = 1:sample_num
+    [y, Fs] = audioread(PATH_NAME+mat2str(n)+".wav");
     L = length(y(:, 1));
     % t = (0:L-1)/Fs;
 
@@ -13,12 +28,13 @@ for n = 1:14
     Rt1 = zeros(1, num);
     Rt2 = zeros(1, num);
     yf = fft(y);
+    alpha = 0.648;
 
     for iter=1:num
         cc1 = yf(:, 1) .* conj(yf(:, 2));
-        cc1 = cc1 ./ (abs(yf(:, 1)) .* abs(yf(:, 2)));
+        cc1 = cc1 ./ (abs(yf(:, 1)) .* abs(yf(:, 2))).^ alpha;
         cc2 = yf(:, 2) .* conj(yf(:, 1));
-        cc2 = cc2 ./ (abs(yf(:, 1)) .* abs(yf(:, 2)));
+        cc2 = cc2 ./ (abs(yf(:, 1)) .* abs(yf(:, 2))).^ alpha;
         ks = 0:length(yf(:, 1))-1;
         es = exp(1i*2*pi*(iter-1).*ks/length(ks));
         cc1 = cc1 .* es.';
@@ -29,22 +45,31 @@ for n = 1:14
 
     tau = (-num+1:num-1)/Fs;
     Rt = [Rt2(end:-1:2), Rt1];
-    subplot(4, 4, n)
-    plot(tau, abs(Rt))
+    if(TRAINING_SET == 1)
+        subplot(4, 4, n)
+        plot(tau, abs(Rt))
+        xlabel(n)
+    end
     [rs, idx] = sort(abs(Rt), 'descend');
     topmany = 3;
     idx_top = tau(idx(1:topmany));
     rt_top = Rt(idx(1:topmany));
     avg = dot(idx_top, rt_top) / sum(rt_top);
-    deg(n) = abs(acosd(avg * 3430));
+    deg_init(n) = real(avg * 3430);
+    deg(n) = abs(acosd(deg_init(n)));
     if(deg(n) > 180)
         deg(n) = 180;
     end
-    xlabel(n)
 end
 
-ground_truth'
-deg
-e = abs(deg - ground_truth')
-mean(e)
-(sum(e)-max(e))/(length(e)-1)
+
+if(TRAINING_SET == 1)
+    ground_truth'
+    deg
+    e = abs(ground_truth' - deg)
+    err_mean = mean(e)
+    err_mean_without_outlier = (sum(e)-max(e))/(length(e)-1)
+else
+    dlmwrite("result.txt", deg,'delimiter',  '\n', 'precision', '%.7f');
+    disp("Output file 'result.txt' created.");
+end
