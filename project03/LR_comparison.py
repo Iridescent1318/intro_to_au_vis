Fixed a bug that gives incorrect gradient. Generated final classification files.

project03/LR_comparison.py
@@ -4,39 +4,41 @@ from matplotlib import pyplot as plt
 from LogRegression import score, cross_validation, LogRegression
 
 if __name__ == '__main__':
-    chara_vectors_train = np.ones((200, 13))
-    chara_results_train = np.ones(200)
+    im_xs_train = np.ones((200, 13))
+    im_ys_train = np.ones(200)
 
     for i in range(200):
         if i <= 99:
-            chara_vectors_train[i, :] = np.load("./dataset/train/positive/{}/feat.npy".format(i))
-            chara_results_train[i] = 1
+            im_xs_train[i, :] = np.load("./dataset/train/positive/{}/feat.npy".format(i))
+            im_ys_train[i] = 1
         else:
-            chara_vectors_train[i, :] = np.load("./dataset/train/negative/{}/feat.npy".format(i-100))
-            chara_results_train[i] = 0
+            im_xs_train[i, :] = np.load("./dataset/train/negative/{}/feat.npy".format(i-100))
+            im_ys_train[i] = 0
 
-    k = 4
+    k = 8
     cv_num = 1
-    epoch_num = 800
+    max_epoch = 1000
     loss_visual_step = 10
 
     prec_bgd = np.zeros(cv_num)
     prec_newton = np.zeros(cv_num)
     prec_skt = np.zeros(cv_num)
     for cn in range(cv_num):
-        x_train, y_train, x_cvtest, y_cvtest = cross_validation(chara_vectors_train, chara_results_train, k)
-        lr_bgd = LogRegression(chara_vectors_train.shape[1])
-        lr_newton = LogRegression(chara_vectors_train.shape[1])
+        x_train, y_train, x_cvtest, y_cvtest = cross_validation(im_xs_train, im_ys_train, k)
+        lr_bgd = LogRegression(im_xs_train.shape[1])
+        lr_newton = LogRegression(im_xs_train.shape[1])
         skt_lr = LogisticRegression(random_state=0, solver='liblinear').fit(x_train, y_train)
-        loss_bgd = lr_bgd.fit(x_train, y_train, epoch_num, 0.08, 1e-4, 'bgd', loss_visual_step)
-        loss_newton = lr_newton.fit(x_train, y_train, epoch_num, 0.08, 1e-4, 'newton', loss_visual_step)
+        loss_bgd = lr_bgd.fit(x_train, y_train, max_epoch, 0.18, 1e-4, 'bgd', loss_visual_step)
+        loss_newton = lr_newton.fit(x_train, y_train, max_epoch, 0.18, 1e-4, 'newton', loss_visual_step)
         y_pred_bgd = lr_bgd.predict(x_cvtest)
         y_pred_newton = lr_newton.predict(x_cvtest)
         prec_bgd[cn] = score(y_pred_bgd, y_cvtest)
         prec_newton[cn] = score(y_pred_newton, y_cvtest)
         prec_skt[cn] = skt_lr.score(x_cvtest, y_cvtest)
         plt.plot((np.arange(len(loss_bgd))+1) * loss_visual_step, loss_bgd)
+        plt.legend("BGD")
         plt.plot((np.arange(len(loss_newton)) + 1) * loss_visual_step, loss_newton)
+        plt.legend("Newton")
 
     print("sklearn LR accuracy:      {}".format(prec_skt))
     print("BGD Precision:            {}".format(prec_bgd))

project03/LR_first_order.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
             im_xs_train[i, :] = np.load("./dataset/train/negative/{}/feat.npy".format(i-100))
             im_ys_train[i] = 0
 
-    k = 8
-    cv_num = 10
+    k = 5
+    cv_num = 5
     max_epoch = 500
     loss_visual_step = 10
         
@@ -29,10 +29,13 @@ if __name__ == '__main__':
             x_train, y_train, x_cvtest, y_cvtest = cross_validation(im_xs_train, im_ys_train, k)
             lr = LogRegression(im_xs_train.shape[1])
             skt_lr = LogisticRegression(random_state=0, solver='liblinear').fit(x_train, y_train)
-            loss = lr.fit(x_train, y_train, max_epoch, 0.08, 1e-4, 'bgd', loss_visual_step)
+            loss = lr.fit(x_train, y_train, max_epoch, 0.18, 1e-4, 'newton', loss_visual_step)
             y_pred = lr.predict(x_cvtest)
+            y_pred_skt = skt_lr.predict(x_cvtest)
             prec[cn] = score(y_pred, y_cvtest)
             prec_skt[cn] = skt_lr.score(x_cvtest, y_cvtest)
+            print("Prediction: {}".format(y_pred))
+            print("Real:       {}".format(y_cvtest))
             plt.plot((np.arange(len(loss))+1) * loss_visual_step, loss)
     
         print("sklearn LR accuracy: {}".format(prec_skt))
@@ -45,14 +48,19 @@ if __name__ == '__main__':
         im_ys_test = np.load("./test_result.npy")
 
         for i in range(100):
-            im_xs_train[i, :] = np.load("./dataset/test/{}/feat.npy".format(i))
+            im_xs_test[i, :] = np.load("./dataset/test/{}/feat.npy".format(i))
 
         lr = LogRegression(im_xs_train.shape[1])
         loss = lr.fit(im_xs_train, im_ys_train, max_epoch, l_rate=0.08, tol=1e-4, method='bgd')
-        y_pred = lr.predict(im_xs_test)
-        precision = score(y_pred, im_ys_test)
-        print("Precision: {}".format(precision))
+        y_pred_bgd = lr.predict(im_xs_test)
+        prec_bgd = score(y_pred_bgd, im_ys_test)
+        print(y_pred_bgd)
+        print("Precision: {}".format(prec_bgd))
 
         skt_lr = LogisticRegression(random_state=0, solver='liblinear').fit(im_xs_train, im_ys_train)
-        precision = skt_lr.score(im_xs_test, im_ys_test)
-        print("Precision: {}".format(precision))
+        y_pred_skt = skt_lr.predict(im_xs_test)
+        prec_skt = skt_lr.score(im_xs_test, im_ys_test)
+        print(y_pred_skt)
+        print(im_ys_test)
+        print("Precision: {}".format(prec_skt))
+        np.save("A_1st_order.npy", y_pred_bgd)

project03/LR_second_order.py
@@ -45,14 +45,19 @@ if __name__ == '__main__':
         im_ys_test = np.load("./test_result.npy")
 
         for i in range(100):
-            im_xs_train[i, :] = np.load("./dataset/test/{}/feat.npy".format(i))
+            im_xs_test[i, :] = np.load("./dataset/test/{}/feat.npy".format(i))
 
         lr = LogRegression(im_xs_train.shape[1])
-        loss = lr.fit(im_xs_train, im_ys_train, max_epoch, l_rate=0.08, tol=1e-4, method='newton')
-        y_pred = lr.predict(im_xs_test)
-        precision = score(y_pred, im_ys_test)
-        print("Precision: {}".format(precision))
+        loss = lr.fit(im_xs_train, im_ys_train, max_epoch, l_rate=0.18, tol=1e-4, method='newton')
+        y_pred_bgd = lr.predict(im_xs_test)
+        prec_bgd = score(y_pred_bgd, im_ys_test)
+        print(y_pred_bgd)
+        print("Precision: {}".format(prec_bgd))
 
         skt_lr = LogisticRegression(random_state=0, solver='newton-cg').fit(im_xs_train, im_ys_train)
-        precision = skt_lr.score(im_xs_test, im_ys_test)
-        print("Precision: {}".format(precision))
+        y_pred_skt = skt_lr.predict(im_xs_test)
+        prec_skt = skt_lr.score(im_xs_test, im_ys_test)
+        print(y_pred_skt)
+        print(im_ys_test)
+        print("Precision: {}".format(prec_skt))
+        np.save("A_2nd_order.npy", y_pred_bgd)

project03/LogRegression.py
@@ -41,11 +41,13 @@ class LogRegression:
                     y_hat = self.get_sigmoid(xs[j, :])
                     w_grad += (y_hat - ys[j]) * xs[j, :]
                     b_grad += (y_hat - ys[j])
+                w_grad /= xs.shape[0]
+                b_grad /= xs.shape[0]
                 self.weights = self.weights - l_rate * w_grad
                 self.bias = self.bias - l_rate * b_grad
                 for j in range(xs.shape[0]):
                     dp = np.dot(xs[j, :], self.weights)
-                    if dp + self.bias > 10:
+                    if dp + self.bias > 20:
                         loss += (dp + self.bias) * (1 - ys[j])
                     else:
                         loss += math.log(1+math.exp(dp + self.bias)) - (dp + self.bias) * ys[j]
@@ -69,6 +71,8 @@ class LogRegression:
                     w_hessian += y_hat * (1 - y_hat) * np.matmul(xs[j, :].T, xs[j, :])
                     b_grad += (y_hat - ys[j])
                     b_hessian += y_hat * (1 - y_hat)
+                w_grad /= xs.shape[0]
+                b_grad /= xs.shape[0]
                 w_hessian_inv = np.linalg.inv(w_hessian)
                 self.weights += -l_rate * np.matmul(w_grad, w_hessian_inv.T)
                 self.bias += -l_rate * b_grad / b_hessian
@@ -105,7 +109,6 @@ class LogRegression:
                 if en % vis_epoch_step == 0:
                     print("--------------------------------Epoch:{}--------------------------------".format(en))
                     print("weights: {} \n bias:   {}".format(self.weights, self.bias))
-                    print("w_grad:  {} \n b_grad: {}".format(w_grad, b_grad))
                     print("loss: {}".format(cur_loss))
                     print("------------------------------------------------------------------------")
         return loss_list

project03/SVM.py
@@ -1,7 +1,5 @@
 import numpy as np
-import librosa
 import librosa.display
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
 from LogRegression import cross_validation, score
@@ -70,10 +68,12 @@ if __name__ == '__main__':
         print("Precision of skt: {}".format(prec_skt))
         print("Mean Precision of SVM: {}".format(np.mean(prec_svm)))
         print("Mean Precision of skt: {}".format(np.mean(prec_skt)))
-
     else:
-
         clf = svm.SVC(gamma='scale')
         clf.fit(au_xs_train, au_ys_train)
+        y_pred = clf.predict(au_xs_test)
         precision = clf.score(au_xs_test, au_ys_test)
         print("Precision: {}".format(precision))
+        print("Prediction: {}".format(y_pred))
+        print("Real:       {}".format(au_ys_test))
+        np.save("B.npy", y_pred)