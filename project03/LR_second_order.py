d0892762b99634ee9024679c0cc0b8d8793b2c2d
Iridescent1318 <lzty19981106@qq.com>
2019-12-21 11:14:02 +0800

    update

--- project03/LR_second_order.py
+++ project03/LR_second_order.py
@@ -3,37 +3,56 @@ from sklearn.linear_model import LogisticRegression
 from matplotlib import pyplot as plt
 from LogRegression import score, cross_validation, LogRegression
 
+CROSS_VALID_MODE = 0
+
 if __name__ == '__main__':
-    chara_vectors_train = np.ones((200, 13))
-    chara_results_train = np.ones(200)
+    im_xs_train = np.ones((200, 13))
+    im_ys_train = np.ones(200)
 
     for i in range(200):
         if i <= 99:
-            chara_vectors_train[i, :] = np.load("./dataset/train/positive/{}/feat.npy".format(i))
-            chara_results_train[i] = 1
+            im_xs_train[i, :] = np.load("./dataset/train/positive/{}/feat.npy".format(i))
+            im_ys_train[i] = 1
         else:
-            chara_vectors_train[i, :] = np.load("./dataset/train/negative/{}/feat.npy".format(i-100))
-            chara_results_train[i] = 0
+            im_xs_train[i, :] = np.load("./dataset/train/negative/{}/feat.npy".format(i-100))
+            im_ys_train[i] = 0
 
     k = 8
     cv_num = 10
-    epoch_num = 500
+    max_epoch = 500
     loss_visual_step = 10
 
-    prec = np.zeros(cv_num)
-    prec_skt = np.zeros(cv_num)
-    for cn in range(cv_num):
-        x_train, y_train, x_cvtest, y_cvtest = cross_validation(chara_vectors_train, chara_results_train, k)
-        lr = LogRegression(chara_vectors_train.shape[1])
-        skt_lr = LogisticRegression(random_state=0, solver='newton-cg').fit(x_train, y_train)
-        loss = lr.fit(x_train, y_train, epoch_num, 0.08, 1e-4, 'newton', loss_visual_step)
-        y_pred = lr.predict(x_cvtest)
-        prec[cn] = score(y_pred, y_cvtest)
-        prec_skt[cn] = skt_lr.score(x_cvtest, y_cvtest)
-        plt.plot((np.arange(len(loss))+1) * loss_visual_step, loss)
+    if CROSS_VALID_MODE:    
+        prec = np.zeros(cv_num)
+        prec_skt = np.zeros(cv_num)
+        for cn in range(cv_num):
+            x_train, y_train, x_cvtest, y_cvtest = cross_validation(im_xs_train, im_ys_train, k)
+            lr = LogRegression(im_xs_train.shape[1])
+            skt_lr = LogisticRegression(random_state=0, solver='newton-cg').fit(x_train, y_train)
+            loss = lr.fit(x_train, y_train, max_epoch, 0.08, 1e-4, 'newton', loss_visual_step)
+            y_pred = lr.predict(x_cvtest)
+            prec[cn] = score(y_pred, y_cvtest)
+            prec_skt[cn] = skt_lr.score(x_cvtest, y_cvtest)
+            plt.plot((np.arange(len(loss))+1) * loss_visual_step, loss)
+    
+        print("sklearn LR accuracy: {}".format(prec_skt))
+        print("Precision:           {}".format(prec))
+        print("Mean sklearn LR accuracy: {:.4f}".format(np.mean(prec_skt)))
+        print("Mean precision:           {:.4f}".format(np.mean(prec)))
+        plt.show()
+    else:
+        im_xs_test = np.ones((100, 13))
+        im_ys_test = np.load("./test_result.npy")
 
-    print("sklearn LR accuracy: {}".format(prec_skt))
-    print("Precision:           {}".format(prec))
-    print("Mean sklearn LR accuracy: {:.4f}".format(np.mean(prec_skt)))
-    print("Mean precision:           {:.4f}".format(np.mean(prec)))
-    plt.show()
+        for i in range(100):
+            im_xs_train[i, :] = np.load("./dataset/test/{}/feat.npy".format(i))
+
+        lr = LogRegression(im_xs_train.shape[1])
+        loss = lr.fit(im_xs_train, im_ys_train, max_epoch, l_rate=0.08, tol=1e-4, method='newton')
+        y_pred = lr.predict(im_xs_test)
+        precision = score(y_pred, im_ys_test)
+        print("Precision: {}".format(precision))
+
+        skt_lr = LogisticRegression(random_state=0, solver='newton-cg').fit(im_xs_train, im_ys_train)
+        precision = skt_lr.score(im_xs_test, im_ys_test)
+        print("Precision: {}".format(precision))
--- project03/SVM.py
+++ project03/SVM.py
@@ -7,6 +7,7 @@ from sklearn import svm
 from LogRegression import cross_validation, score
 
 FEAT_EXT_MODE = 0
+CROSS_VALID_MODE = 0
 
 if __name__ == '__main__':
     num_mfcc = 1
@@ -14,7 +15,9 @@ if __name__ == '__main__':
         au_test, au_sr = librosa.load("./dataset/train/positive/0/audio.wav")
         au_test = librosa.feature.mfcc(au_test, au_sr, n_mfcc=num_mfcc).reshape(-1)
         au_xs_train = np.ones((200, au_test.shape[0]))
+        au_xs_test = np.ones((100, au_test.shape[0]))
         au_ys_train = np.ones(200)
+        au_ys_test = np.load("./test_result.npy")
 
         for i in range(200):
             if i <= 99:
@@ -25,36 +28,52 @@ if __name__ == '__main__':
             else:
                 au, sr = librosa.load("./dataset/train/negative/{}/audio.wav".format(i-100))
                 au_mfcc = librosa.feature.mfcc(au, sr, n_mfcc=num_mfcc).reshape(-1)
-                au_xs_train[i, :] = librosa.feature.mfcc(au, sr).reshape(-1)
+                au_xs_train[i, :] = au_mfcc
                 au_ys_train[i] = 0
 
-        np.save("au_feat_xs.npy", au_xs_train)
-        np.save("au_feat_ys.npy", au_ys_train)
+        for i in range(100):
+            au, sr = librosa.load("./dataset/test/{}/audio.wav".format(i))
+            au_mfcc = librosa.feature.mfcc(au, sr, n_mfcc=num_mfcc).reshape(-1)
+            au_xs_test[i, :] = au_mfcc
+
+        np.save("au_xs_train.npy", au_xs_train)
+        np.save("au_ys_train.npy", au_ys_train)
+        np.save("au_xs_test.npy", au_xs_test)
+
     else:
-        au_xs_train = np.load("au_feat_xs.npy")
-        au_ys_train = np.load("au_feat_ys.npy")
+        au_xs_train = np.load("au_xs_train.npy")
+        au_ys_train = np.load("au_ys_train.npy")
+        au_xs_test = np.load("au_xs_test.npy")
+        au_ys_test = np.load("./test_result.npy")
 
     k = 2
     cv_num = 10
 
-    prec_svm = np.zeros(cv_num)
-    prec_skt = np.zeros(cv_num)
+    if CROSS_VALID_MODE:
+        prec_svm = np.zeros(cv_num)
+        prec_skt = np.zeros(cv_num)
 
-    for cn in range(cv_num):
-        x_train, y_train, x_cvtest, y_cvtest = cross_validation(au_xs_train, au_ys_train, k)
-        au_clf = svm.SVC(gamma='scale')
-        au_clf.fit(x_train, y_train)
-        skt_lr = LogisticRegression(random_state=0, solver='liblinear').fit(x_train, y_train)
-        y_pred_svm = au_clf.predict(x_cvtest)
-        y_pred_skt = skt_lr.predict(x_cvtest)
-        print("Predictions of SVM: {}".format(y_pred_svm))
-        print("Predictions of skt: {}".format(y_pred_skt))
-        print("Real:               {}".format(y_cvtest))
-        prec_svm[cn] = score(y_pred_svm, y_cvtest)
-        prec_skt[cn] = score(y_pred_skt, y_cvtest)
+        for cn in range(cv_num):
+            x_train, y_train, x_cvtest, y_cvtest = cross_validation(au_xs_train, au_ys_train, k)
+            au_clf = svm.SVC(gamma='scale')
+            au_clf.fit(x_train, y_train)
+            skt_lr = LogisticRegression(random_state=0, solver='liblinear').fit(x_train, y_train)
+            y_pred_svm = au_clf.predict(x_cvtest)
+            y_pred_skt = skt_lr.predict(x_cvtest)
+            print("Predictions of SVM: {}".format(y_pred_svm))
+            print("Predictions of skt: {}".format(y_pred_skt))
+            print("Real:               {}".format(y_cvtest))
+            prec_svm[cn] = score(y_pred_svm, y_cvtest)
+            prec_skt[cn] = score(y_pred_skt, y_cvtest)
 
-    print("Precision of SVM: {}".format(prec_svm))
-    print("Precision of skt: {}".format(prec_skt))
-    print("Mean Precision of SVM: {}".format(np.mean(prec_svm)))
-    print("Mean Precision of skt: {}".format(np.mean(prec_skt)))
+        print("Precision of SVM: {}".format(prec_svm))
+        print("Precision of skt: {}".format(prec_skt))
+        print("Mean Precision of SVM: {}".format(np.mean(prec_svm)))
+        print("Mean Precision of skt: {}".format(np.mean(prec_skt)))
 
+    else:
+
+        clf = svm.SVC(gamma='scale')
+        clf.fit(au_xs_train, au_ys_train)
+        precision = clf.score(au_xs_test, au_ys_test)
+        print("Precision: {}".format(precision))
